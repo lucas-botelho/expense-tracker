001d450042d86e1b39706ad9251672197979b507
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2024-01-24 18:36:12 +0000

    prisma setup

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+        <>
+            <title>Expense Tracker</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta name="description" content="Expense Tracker" />
+            <link rel="icon" href="/favicon.ico" />
+        </>);
+}
